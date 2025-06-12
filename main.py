41cec7b5e8ac80ddb432db16cbbc7dd2f12d1d99
Fayzullo <fayzullomaksudov98@gmail.com>
2025-06-12 16:17:04 +0500

    new files
    New lessons from Mohir.dev

---
+++ main.py
@@ -0,0 +1,9 @@
+#radius = 20 int (целое число)
+#PI = 3.14159 float (дробное число)
+#diametr = 2*radius
+#print("Aylana uzunligi=", PI*diametr)
+#print(type(diametr))
+#str(radius) ("str" = текст)
+#print(type(radius))
+#radius = str(radius) при этой командой мы можем перевести "int(целое число)" на "str(текст)"
+#print(type(radius))
